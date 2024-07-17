dc5bee0c8aac082a409b307340d0bc458db67ce1
EC2 Default User <ec2-user@ip-172-31-90-204.ec2.internal>
2024-07-17 10:42:59 +0000

    .

--- streamlit_chart.py
+++ streamlit_chart.py
@@ -28,7 +28,7 @@ gaze_data_headers = [data[0] for data in gaze_data_headers]
 selected_gaze_data = st.selectbox('Select Gaze Data', gaze_data_headers)
 
 # Query data for the selected gaze data type, ordered by ID
-query = f"SELECT X, Y FROM GazeData WHERE header = '{selected_gaze_data}' AND state = 0 ORDER BY time ASC"
+query = f"SELECT X/{SCREEN_WIDTH} AS X, Y/{SCREEN_HEIGHT} AS Y FROM GazeData WHERE header = '{selected_gaze_data}' AND state = 0 ORDER BY time ASC"
 
 data = pd.read_sql_query(query, conn)
 conn.close()
@@ -41,14 +41,11 @@ count = 0
 
 # Process data for plotting
 for index, row in data.iterrows():
-    #x = row['X']
-    #y = 1 - row['Y']  # Reverse y-coordinate
-    x = row['X'] / SCREEN_WIDTH
-    y = 1 - (row['Y'] / SCREEN_HEIGHT) 
+    x = row['X']
+    y = 1 - row['Y']  # Reverse y-coordinate
     count += 1
 
     # Check engagement in the specified area
-    #if 0.3 < x < 0.6 and 0.25 < y < 0.6:
     if 0.3 < x < 0.6 and 0.25 < y < 0.6:
         engaged += 1
 
@@ -89,12 +86,12 @@ st.write(data.describe())
 fig, axes = plt.subplots(1, 2, figsize=(20, 10))
 
 # Box plot for X
-sns.boxplot(ax=axes[0], x=data['X']/SCREEN_WIDTH, color='lightblue')
+sns.boxplot(ax=axes[0], x=data['X'], color='lightblue')
 axes[0].set_title('Box Plot of X', fontsize=25)
 axes[0].set_xlabel('X', fontsize=20)
 
 # Box plot for Y
-sns.boxplot(ax=axes[1], x=data['Y']/SCREEN_HEIGHT, color='lightgreen')
+sns.boxplot(ax=axes[1], x=data['Y'], color='lightgreen')
 axes[1].set_title('Box Plot of Y', fontsize=25)
 axes[1].set_xlabel('Y', fontsize=20)
 
