efd61624e16f75e6ea2dc342c8293f1bc3ef32a9
EC2 Default User <ec2-user@ip-172-31-90-204.ec2.internal>
2024-07-17 08:56:02 +0000

    .

--- run_main.sh
+++ run_main.sh
@@ -1,3 +1,7 @@
+kill -9 `ps -ef|grep "python3 /home/ec2-user/git/CloudComputing_GazeRecorderDemo/app.py"| awk 'NR==1 {print $2}'`
 kill -9 `ps -ef|grep "python3 app.py"| awk 'NR==1 {print $2}'`
+kill -9 `ps -ef|grep "streamlit run streamlit_chart.py"| awk 'NR==1 {print $2}'`
 nohup python3 app.py &
+nohup streamlit run streamlit_chart.py &
 ps -ef |grep "python3 app.py"
+ps -ef |grep "streamlit run streamlit_chart.py"
